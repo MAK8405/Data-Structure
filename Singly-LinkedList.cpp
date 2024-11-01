23efa9c2177e204b579759a1521166ba41fefc76
Mahdi Ahmadi <mahdiahmadi2005kf@gmail.com>
2024-11-01 20:26:01 +0330

    initial commit

---
+++ Singly-LinkedList.cpp
@@ -0,0 +1,19 @@
+// LinkedList with 3 elements
+#include <iostream>
+using namespace std;
+
+class Node
+{
+public:
+    int Value;
+    Node *Next;
+};
+
+int main()
+{
+    Node *head = new Node(); // create pointer and alocate space for first node
+    Node *second = new Node();
+    Node *third = new Node();
+
+    return 0;
+}
