2cf9e7b47dc170b11a6295b79bfad86d1ff5e062
Mahdi Ahmadi <mahdiahmadi2005kf@gmail.com>
2024-11-02 23:52:55 +0330

    Finish the Code

--- Singly-LinkedList.cpp
+++ Singly-LinkedList.cpp
@@ -9,11 +9,29 @@ public:
     Node *Next;
 };
 
+void printList(Node *n)
+{
+    while (n != NULL)
+    {
+        cout << n->Value << endl;
+        n = n->Next;
+    }
+}
+
 int main()
 {
-    Node *head = new Node(); // create pointer and alocate space for first node
+    Node *head = new Node(); // create pointer and alocate space for first node without value
     Node *second = new Node();
     Node *third = new Node();
+
+    head->Value = 1;     // -> this symbol means trying access to the class in case of using pointer
+    head->Next = second; // head element point to the second element
+    second->Value = 2;
+    second->Next = third;
+    third->Value = 3;
+    third->Next = NULL;
+
+    printList(head);
 
     return 0;
 }
